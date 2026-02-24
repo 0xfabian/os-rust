d6fc26dbda554ada049ef3b9f930122bb1589888
0xfabian <dumitru_fabian0@yahoo.com>
2026-02-24 23:18:49 +0200

    Basic GDT and IDT setup

--- src/main.rs
+++ src/main.rs
@@ -11,51 +11,151 @@ mod terminal;
 
 use logger::*;
 use pmm::*;
-use requests::MEMORY_MAP_REQUEST;
 use requests::{BASE_REVISION, MP_REQUEST};
-use sync::SpinLock;
-use x86_64::PhysAddr;
+use x86_64::PrivilegeLevel;
+use x86_64::instructions::segmentation::{CS, DS, ES, FS, GS, SS};
 use x86_64::registers::model_specific::GsBase;
+use x86_64::registers::segmentation::Segment;
+use x86_64::structures::gdt::{Descriptor, GlobalDescriptorTable, SegmentSelector};
 use x86_64::structures::idt::InterruptDescriptorTable;
 
 use crate::panic::idle;
 
+#[repr(C)]
+#[derive(Default)]
 struct TlsData {
     cpu_id: u32,
+    idt_addr: u64,
+    gdt_addr: u64,
 }
 
-static IDT: SpinLock<InterruptDescriptorTable> = SpinLock::new(InterruptDescriptorTable::new());
-
 fn cpu_id() -> u32 {
-    // We are in kernel space, GS base should point to TLS data
-    let gs_base = GsBase::read();
-
-    if gs_base.is_null() {
-        panic!("GS base is null, TLS data not set up");
+    let cpu_id: u32;
+    unsafe {
+        core::arch::asm!("mov {0:e}, gs:[0]", out(reg) cpu_id);
     }
+    cpu_id
+}
 
-    let tls_data = unsafe { &*(gs_base.as_u64() as *const TlsData) };
-    tls_data.cpu_id
+fn get_tls() -> &'static mut TlsData {
+    let tls_addr = GsBase::read();
+    unsafe { &mut *(tls_addr.as_u64() as *mut TlsData) }
 }
 
 // Each CPU gets it's own TLS page, where currently only the CPU ID is stored.
 fn setup_tls(cpu: &limine::mp::Cpu) {
     let tls_addr = alloc_frames(1)
-        .map(|frames| phys_to_virt(frames.start))
+        .map(|f| phys_to_virt(f.start))
         .expect("Out of memory");
 
     assert!(core::mem::size_of::<TlsData>() <= 4096);
 
     let tls_data = unsafe { &mut *(tls_addr.as_mut_ptr::<TlsData>()) };
-    *tls_data = TlsData { cpu_id: cpu.id };
+    *tls_data = TlsData::default();
+    tls_data.cpu_id = cpu.id;
 
     GsBase::write(tls_addr);
+}
+
+// The IDT structure fits perfectly in one page,
+// so we allocate one page and store the IDT there.
+fn alloc_idt() -> &'static mut InterruptDescriptorTable {
+    let idt_addr = alloc_frames(1)
+        .map(|f| phys_to_virt(f.start))
+        .expect("Out of memory");
+
+    assert!(core::mem::size_of::<InterruptDescriptorTable>() == 4096);
+
+    let idt = unsafe { &mut *(idt_addr.as_mut_ptr::<InterruptDescriptorTable>()) };
+    // Effectively zero out the supporting page.
+    idt.reset();
+
+    idt
+}
+
+fn setup_idt() {
+    let idt = alloc_idt();
+
+    populate_idt(idt);
+    idt.load();
+
+    let tls = get_tls();
+    tls.idt_addr = idt as *const _ as u64;
+}
+
+fn populate_idt(idt: &mut InterruptDescriptorTable) {
+    unsafe {
+        idt.breakpoint
+            .set_handler_fn(handler)
+            .set_code_selector(KERNEL_CS)
+            .set_privilege_level(PrivilegeLevel::Ring3);
+    }
+
+    // TODO: set up more handlers, at least for the exceptions.
 }
 
 extern "x86-interrupt" fn handler(_stack_frame: x86_64::structures::idt::InterruptStackFrame) {
     // This is unsafe and could deadlock, but for now, it's ok.
     // Eventually, we should use per CPU buffers and a background thread to print them.
     println!("Breakpoint Exception Handler called on CPU {}", cpu_id());
+    idle();
+}
+
+const KERNEL_CS: SegmentSelector = SegmentSelector::new(1, PrivilegeLevel::Ring0);
+const KERNEL_DS: SegmentSelector = SegmentSelector::new(2, PrivilegeLevel::Ring0);
+const USER_DS: SegmentSelector = SegmentSelector::new(3, PrivilegeLevel::Ring3);
+const USER_CS: SegmentSelector = SegmentSelector::new(4, PrivilegeLevel::Ring3);
+
+// Compared to the IDT, the GDT is usually tiny,
+// still, we allocate one page for it, to keep things simple.
+fn alloc_gdt() -> &'static mut GlobalDescriptorTable<7> {
+    let gdt_addr = alloc_frames(1)
+        .map(|f| phys_to_virt(f.start))
+        .expect("Out of memory");
+
+    // We need 7 entries:
+    //  1. null entry
+    //  2. kernel code
+    //  3. kernel data
+    //  4. user data (not code, due to sysret working in both 32 and 64 bit)
+    //  5. user code
+    //  6-7. TSS
+    assert!(core::mem::size_of::<GlobalDescriptorTable<7>>() < 4096);
+
+    let gdt = unsafe { &mut *(gdt_addr.as_mut_ptr::<GlobalDescriptorTable<7>>()) };
+    *gdt = GlobalDescriptorTable::<7>::empty();
+
+    gdt
+}
+
+fn setup_gdt() {
+    let gdt = alloc_gdt();
+
+    // Null entry is already set.
+    gdt.append(Descriptor::kernel_code_segment());
+    gdt.append(Descriptor::kernel_data_segment());
+    gdt.append(Descriptor::user_data_segment());
+    gdt.append(Descriptor::user_code_segment());
+    // TODO: set up TSS and it's descriptor.
+
+    gdt.load();
+
+    // Update the segment registers accordingly.
+    unsafe {
+        CS::set_reg(KERNEL_CS);
+        DS::set_reg(KERNEL_DS);
+        SS::set_reg(KERNEL_DS);
+        ES::set_reg(KERNEL_DS);
+        FS::set_reg(KERNEL_DS);
+
+        // Turns out setting GS resets GS base and kernel GS base...
+        let gs_base = GsBase::read();
+        GS::set_reg(KERNEL_DS);
+        GsBase::write(gs_base);
+    }
+
+    let tls = get_tls();
+    tls.gdt_addr = gdt as *const _ as u64;
 }
 
 extern "C" fn common_entry(cpu: &limine::mp::Cpu) -> ! {
@@ -71,33 +171,16 @@ extern "C" fn common_entry(cpu: &limine::mp::Cpu) -> ! {
     setup_tls(cpu);
     // Only from this point on, the CPU ID can be obtained using the `cpu_id()` function.
 
-    unsafe {
-        IDT.lock().load_unsafe();
-    }
+    // This does a couple of this:
+    //  - sets up the GDT structure per CPU
+    //  - sets up the segment registers
+    setup_gdt();
+
+    // Do it after the GDT, so that we have the right segment registers.
+    setup_idt();
 
     println!("CPU {} entering idle loop", cpu_id());
     idle();
-}
-
-// Temporary function used to understand the system's state during early boot.
-fn which_mem_region(addr: PhysAddr) -> &'static str {
-    let regions = MEMORY_MAP_REQUEST.get_response().unwrap().entries();
-
-    for entry in regions {
-        if addr.as_u64() >= entry.base && addr.as_u64() < entry.base + entry.length {
-            return match entry.entry_type {
-                limine::memory_map::EntryType::USABLE => "USABLE",
-                limine::memory_map::EntryType::RESERVED => "RESERVED",
-                limine::memory_map::EntryType::ACPI_RECLAIMABLE => "ACPI_RECLAIMABLE",
-                limine::memory_map::EntryType::ACPI_NVS => "ACPI_NVS",
-                limine::memory_map::EntryType::BAD_MEMORY => "BAD_MEMORY",
-                limine::memory_map::EntryType::BOOTLOADER_RECLAIMABLE => "BOOTLOADER_RECLAIMABLE",
-                _ => "UNKNOWN",
-            };
-        }
-    }
-
-    "NOT FOUND"
 }
 
 #[unsafe(no_mangle)]
@@ -107,8 +190,6 @@ extern "C" fn kmain() -> ! {
     assert!(logger_init());
 
     pmm_init();
-
-    IDT.lock().breakpoint.set_handler_fn(handler);
 
     let mp_resp = MP_REQUEST.get_response().unwrap();
     let mut bsp_cpu = None;
